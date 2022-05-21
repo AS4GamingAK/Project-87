12ae0572fa122f0455fc0f7fd9f321badf044d8b
Amol Srivastava <86219561+AS4GamingAK@users.noreply.github.com>
2022-05-21 13:24:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
-var canvas=new fabric.Canvas('myCanvas');
+var canvas = new fabric.Canvas("myCanvas");
+
  block_y=1;
  block_x=1;
 
@@ -10,19 +11,17 @@ var block_image_object= "";
 
 function new_image(get_image)
 {
-	fabric.Image.formURL(get_image, function(Img) {
-		block_image_object = Img;
-
-		block_image_object.scaleToWidth(block_image_width);
-		block_image_object.scaleToHeight(block_image_height);
-		block_image_object.set({
-			top:block_y,
-			left:block_x
-		});
-		canvas.add(block_image_object);
-	});
+	fabric.Image.fromURL(get_image, function (Img){
+        block_image_object = Img;
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set({
+            top:block_y,
+            left:block_x
+        });
+        canvas.add(block_image_object);
+    });
 }
-
 window.addEventListener("keydown", my_keydown);
 
 function my_keydown(e)
@@ -32,37 +31,32 @@ console.log(keyPressed);
 
 	if(keyPressed == '82')
 	{
-		new_image('rr1.png');
-		console.log("r");
+		new_image("rr1.png");
+		console.log("r Key Pressed");
 	}
-
 	if(keyPressed == '71')
 	{
 		block_x = 200;
-		new_image('gr.png');
-		console.log("g");
+		new_image("gr.png");
+		console.log("g Key Pressed");
 	}
 	
 	if(keyPressed == '89')
 	{
 		block_x =350;
-		new_image('yr.png');
-		console.log("y");
+		new_image("yr.png");
+		console.log("y Key Pressed");
 	}
-	
 	if(keyPressed == '80')
 	{
 		block_x = 600;
-		new_image('pr.png');
-		console.log("p");
+		new_image("pr.png");
+		console.log("p Key Pressed");
 	}
-
 	if(keyPressed == '66')
 	{
 		block_x = 700;
-		new_image('br.png');
-		console.log("b");
+		new_image("br.png");
+		console.log("b Key Pressed");
 	}
-	
 }
-
